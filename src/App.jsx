0d04c463157b4ca2478f98239315f64bdd89cee0
muhammed safnad <muhammedsafnadck5@gmail.com>
2024-11-09 15:27:59 +0530

    09-11-2024

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Fileupload from './Components/FileUpload/Fileupload'
+
+const App = () => {
+  return (
+    <div>
+      <Fileupload />
+    </div>
+  )
+}
+
+export default App
